7811ea2c1dacce3b30c788ffd16f19f10293fd5e
Keenan Gee <k33nang33@hotmail.co.uk>
2023-05-18 19:08:04 +0100

    Added fade effect to sections

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,45 @@ import Contact from "./components/Contact";
 import Nav from "./components/Nav";
 import ProjectsPicker from "./components/ProjectsPicker";
 import Welcome from "./components/Welcome";
+import { useEffect, useRef } from "react";
 
 const App: React.FC = () => {
+  const sectionRefs = useRef<Array<HTMLElement | null>>([]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      sectionRefs.current.forEach((section) => {
+        if (section) {
+          const sectionTop = section.getBoundingClientRect().top;
+          const windowHeight = window.innerHeight;
+
+          if (sectionTop < windowHeight * 0.8) {
+            section.classList.add("fade-in");
+          }
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <Nav />
       <Welcome />
-      <About />
-      <ProjectsPicker />
-      {/* <GamesPicker /> */}
-      <Contact />
+      <section ref={(el) => sectionRefs.current.push(el)} className="h-screen">
+        <About />
+      </section>
+      <section ref={(el) => sectionRefs.current.push(el)} className="h-screen">
+        <ProjectsPicker />
+      </section>
+      <section ref={(el) => sectionRefs.current.push(el)} className="h-screen">
+        {/* <GamesPicker /> */}
+        <Contact />
+      </section>
     </>
   );
 };
