7657b031cb5535c6b30542da2fbff893687849b7
Keenan Gee <k33nang33@hotmail.co.uk>
2023-06-09 22:18:32 +0100

    added ref to scroll blog page to top

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,15 +1,23 @@
 import { blogData } from "../data/blogData";
 import { useParams } from "react-router-dom";
 import Footer from "./Footer";
+import { useRef, useEffect } from "react";
 
 const BlogPost = () => {
   const { blog_id } = useParams();
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const blog = blogData.find((blog) => blog.id === Number(blog_id));
 
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return (
     <>
-      <div className=" px-[10vw] pt-[20vh] pb-[10vh]">
+      <div ref={containerRef} className=" px-[10vw] pt-[20vh] pb-[10vh]">
         <h1 className="text-3xl text-center pb-10 text-primary">
           {blog?.title}
         </h1>
