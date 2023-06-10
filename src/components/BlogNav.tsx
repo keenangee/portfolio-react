38bf15ab03dc4465144dbd3edc302d02edc5639b
Keenan Gee <k33nang33@hotmail.co.uk>
2023-06-10 11:45:47 +0100

    changed fade in time and added back arrow on blogs

--- src/components/BlogNav.tsx
+++ src/components/BlogNav.tsx
@@ -1,13 +1,21 @@
+import { RiReplyLine } from "react-icons/ri";
+
 const Nav: React.FC = () => {
   return (
     <div
       className={
-        "top-0 fixed w-screen flex row justify-center p-5 bg-darkest z-10"
+        "top-0 fixed w-screen flex row justify-between p-5 bg-darkest z-10"
       }
     >
       <a href="/">
+        <RiReplyLine size={60} className=" cursor-pointer" color="white" />
+      </a>
+      <a href="/">
         <img src="/kg.png" alt="KG" className="w-15 h-14" />
       </a>
+      <div>
+        <RiReplyLine size={60} color="#201a44" />
+      </div>
     </div>
   );
 };
