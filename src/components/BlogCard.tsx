123208153ba80f0053d1242822d32dbca7e0e2d2
Keenan Gee <k33nang33@hotmail.co.uk>
2023-06-13 10:46:44 +0100

    changed font for desktop

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -14,9 +14,11 @@ const BlogCard = ({ id, title, date, content }: BlogData) => {
     <div className="p-2 border-8 border-primary rounded-md w-[15rem] sm:w-[20rem] xl:w-[25rem] h-[50vh] bg-lightest transition-all duration-500">
       <ul className="flex flex-col justify-between h-full w-full text-light">
         <li className="flex items-center justify-center text-center text-black h-[30%] border-b-2">
-          <h2 className="text-[1rem] sm:text-[1.2rem]">{title}</h2>
+          <h2 className="text-[0.9rem] sm:text-[1.2rem]">{title}</h2>
         </li>
-        <li className=" px-4">{preview}</li>
+        <li className="text-[0.8rem] sm:text-[1rem] px-2 sm:px-4 py-2">
+          {preview}
+        </li>
         <li className=" flex flex-row items-end">
           <Link
             to={`/blog/${id}`}
